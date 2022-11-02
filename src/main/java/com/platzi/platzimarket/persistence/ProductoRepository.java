1d4432b7fc8752578fab7f7f2bd452d7fae5b441
Lesterinfo <bravoo.lester@gmail.com>
2022-11-01 21:20:20 -0400

    se implementan Dominios

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -2,13 +2,40 @@ package com.platzi.platzimarket.persistence;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository //Indicando a Sprint que esta es la clase en interactuar con la BD
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+
+    //QUERY METHOD
+    public List<Producto> getByIdCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByASC(idCategoria);
+    }
+
+    public Optional<List<Producto>> gerEscasos(int CantidadStock){
+    //public Optional<List<Producto>> gerEscasos(int CantidadStock, boolean estado){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(CantidadStock,true);
+        //return productoCrudRepository.findByCantidadStockLessThanAndEstado(CantidadStock,estado);
+    }
+
+    public Optional<Producto> getProducto(int IdProducto){
+      return productoCrudRepository.findById(IdProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int IdProducto){
+        productoCrudRepository.deleteById(IdProducto);
+    }
+
 }
--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,15 @@
 package com.platzi.platzimarket.persistence.crud;
 
 import com.platzi.platzimarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true);
+    List<Producto> findByIdCategoriaOrderByASC(int IdCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int CantidadStock, boolean estado);
 }
